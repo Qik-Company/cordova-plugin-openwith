c7e3b44558d6c39d68a05f66a58fbc250e52d544
Hanh Le <duchanhpy2000@gmail.com>
2023-11-27 13:27:08 +0700

    update plugin variable

--- src/ios/ShareExtension/ShareViewController.h
+++ src/ios/ShareExtension/ShareViewController.h
@@ -1,2 +1,2 @@
-#define SHAREEXT_GROUP_IDENTIFIER @"group.__BUNDLE_IDENTIFIER__"
-#define SHAREEXT_URL_SCHEME @"__URL_SCHEME__"
+#define SHAREEXT_GROUP_IDENTIFIER @"group.mt.qik.app.shareextension"
+#define SHAREEXT_URL_SCHEME @"qikmtopenwith"
